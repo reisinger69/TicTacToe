8405e44d43942b046b85c68abc2ae3bc6f4b8105
reisinger69 <basti4772@gmail.com>
2022-11-08 17:58:01 +0100

    umbau auf mehrere Activities

--- app/src/main/java/reisinger/htl/tictactoe/MainActivity.java
+++ app/src/main/java/reisinger/htl/tictactoe/MainActivity.java
@@ -3,12 +3,60 @@ package reisinger.htl.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private boolean isP1 = true;
+
+    TicTacToeLogic tttl = new TicTacToeLogic();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inizialize();
+    }
+
+    void inizialize() {
+        int[] ids = {R.id.bu1, R.id.bu2, R.id.bu3, R.id.bu4, R.id.bu5, R.id.bu6, R.id.bu7, R.id.bu8, R.id.bu9};
+        for (int id : ids) {
+            findViewById(id).setOnClickListener(this);
+        }
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        Button b = (Button) view;
+        if (b.getText().equals(" ")) {
+            if (isP1) {
+                tttl.newBoard(view.getId(), 'x');
+                b.setText("X");
+                isP1 = false;
+            } else {
+                tttl.newBoard(view.getId(), 'o');
+                b.setText("O");
+                isP1 = true;
+            }
+            switch (tttl.checkForWin()) {
+                case -1:
+                    Log.i("MainActivity", "Player 1 wins");
+                    break;
+                case 1:
+                    Log.i("MainActivity", "Player 2 wins");
+                    break;
+                case -2:
+                    Log.i("MainActivity", "No win yet");
+                    break;
+                case -3:
+                    Log.i("MainActivity", "Draw");
+                    break;
+            }
+        }
     }
 }
---
+++ app/src/main/java/reisinger/htl/tictactoe/TicTacToeLogic.java
@@ -0,0 +1,81 @@
+package reisinger.htl.tictactoe;
+
+public class TicTacToeLogic {
+
+    private char[][] board = new char[3][3];
+    private int counter;
+
+    public TicTacToeLogic() {
+        intzialiseBoard();
+    }
+
+    public void intzialiseBoard() {
+        for (int q = 0; q < board.length; q++) {
+            for (int i = 0; i < board.length; i++) {
+                board[q][i] = ' ';
+            }
+        }
+    }
+
+    public void newBoard(int i, char c) {
+        counter++;
+        String temp = (i + "");
+        switch (i) {
+            case R.id.bu1: board[0][0] = c;
+            break;
+            case R.id.bu2: board[1][0] = c;
+                break;
+            case R.id.bu3: board[2][0] = c;
+                break;
+            case R.id.bu4: board[0][1] = c;
+                break;
+            case R.id.bu5: board[1][1] = c;
+                break;
+            case R.id.bu6: board[2][1] = c;
+                break;
+            case R.id.bu7: board[0][2] = c;
+                break;
+            case R.id.bu8: board[1][2] = c;
+                break;
+            case R.id.bu9: board[2][2] = c;
+                break;
+        };
+    }
+
+    public int checkForWin() {
+
+        if (counter==9) {
+            return -3;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            if ((board[i][0] == 'x' && board[i][1] == 'x' &&board[i][2] == 'x')) {
+                return -1;
+            } else if ((board[i][0] == 'o' && board[i][1] == 'o' &&board[i][2] == 'o')) {
+                return 1;
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            if ((board[0][i] == 'x' && board[1][i] == 'x' &&board[2][i] == 'x')) {
+                return -1;
+            } else if ((board[0][i] == 'o' && board[1][i] == 'o' &&board[2][i] == 'o')) {
+                return 1;
+            }
+        }
+
+        if ((board[0][0] == 'x' && board[1][1] == 'x' && board[2][2] == 'x')) {
+            return -1;
+        } else if ((board[0][0] == 'o' && board[1][1] == 'o' && board[2][2] == 'o')) {
+            return 1;
+        }
+
+        if ((board[2][0] == 'x' && board[1][1] == 'x' && board[0][2] == 'x')) {
+            return -1;
+        } else if ((board[2][0] == 'o' && board[1][1] == 'o' &&board[0][2] == 'o')) {
+            return 1;
+        }
+
+        return -2;
+    }
+}
